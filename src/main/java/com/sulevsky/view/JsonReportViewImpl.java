1b9c3ba120d35de0d80ddf835c7d62c0be3a4d1e
Volodymyr Sulevskyi <volodymyr.sulevskyi@opower.com>
2017-03-01 09:35:17 +0200

    JSON output

---
+++ src/main/java/com/sulevsky/view/JsonReportViewImpl.java
@@ -0,0 +1,26 @@
+package com.sulevsky.view;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.sulevsky.model.Report;
+
+public class JsonReportViewImpl implements ReportView {
+    @Override
+    public void showReport(Report report) {
+        System.out.println(generateView(report));
+    }
+
+    @Override
+    public String generateView(Report report) {
+        ObjectMapper objectMapper = new ObjectMapper();
+        String valueAsString = null;
+        try {
+            valueAsString = objectMapper.writeValueAsString(report);
+        }
+        catch (JsonProcessingException e) {
+            e.printStackTrace();
+            valueAsString = "{error:\"Can't serialize\"}";
+        }
+        return valueAsString;
+    }
+}
