ba3a386e99e86c02797f729a9270b87e24aab086
Volodymyr Sulevskyi <volodymyr.sulevskyi@opower.com>
2017-03-01 10:50:27 +0200

    tests

---
+++ src/test/java/com/sulevsky/view/JsonReportViewImplTest.java
@@ -0,0 +1,29 @@
+package com.sulevsky.view;
+
+import com.sulevsky.model.Report;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = "classpath:application-context.xml")
+public class JsonReportViewImplTest {
+
+    @Autowired
+    JsonReportViewImpl reportView;
+
+    @Test
+    public void shouldInjectComponent() {
+        Assert.assertNotNull(reportView);
+    }
+
+    @Test
+    public void shouldReturnEmptyArrayOnEmptyReport() {
+        String generatedView = reportView.generateView(new Report());
+        String expected = "{\"entries\":[]}";
+        Assert.assertEquals(expected, generatedView);
+    }
+}
